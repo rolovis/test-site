1626e0d827335d80dff719f73422106e3bd5a6f3
Matt Long <rolovis@gmail.com>
2017-05-10 19:26:10 -0700

    added a nifty slideshow

--- main-page/js/script.js
+++ main-page/js/script.js
@@ -5,3 +5,18 @@ function changeActive(tabName) {
 	}
 	document.getElementById(tabName).style.display = 'block';
 }
+
+function slideShow(boxId, start) {
+  var box = document.getElementById(boxId), 
+    slides = document.getElementsByClassName('slide');
+
+  box.classList.remove('slide');
+  void box.offsetWidth;
+  box.classList.add('slide');
+  if (start == slides.length) { start = 0; }
+  box.style.backgroundImage = 'url(' + slides[start++ % slides.length].
+    getElementsByTagName('img')[0].getAttribute('src') + ')';
+  box.style.backgroundSize = '100% auto';
+  setTimeout(slideShow, 2500, boxId, start);
+
+}
